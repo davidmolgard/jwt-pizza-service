4316e8a52881d04f08f499ed8c0ecb1c8b3274a0
davidmolgard <molgarddavid@gmail.com>
2024-10-01 23:38:33 -0600

    allowed no stores if none added yet

--- src/routes/franchiseRouter.test.js
+++ src/routes/franchiseRouter.test.js
@@ -4,6 +4,7 @@ const { DB, Role } = require('../database/database.js');
 
 let adminAuthToken;
 let adminId;
+let adminEmail;
 
 function randomName() {
     return Math.random().toString(36).substring(2, 12);
@@ -27,9 +28,35 @@ beforeAll(async () => {
   const loginRes = await request(app).put('/api/auth').send(user);
   adminAuthToken = loginRes.body.token;
   adminId = loginRes.body.user.id;
+  adminEmail = loginRes.body.user.email;
 });
 
+test('create franchise', async () => {
+    const franchiseName = randomName();
+    const newFranchise = {
+        name: `${franchiseName}`,
+        admins: [{ email: `${adminEmail}` }]
+    };
 
+    const createFranchiseRes = await request(app)
+        .post('/api/franchise')
+        .set('Authorization', `Bearer ${adminAuthToken}`)
+        .set('Content-Type', 'application/json')
+        .send(newFranchise);
+
+    expect(createFranchiseRes.status).toBe(200); // Check for created status
+    expect(createFranchiseRes.body).toMatchObject({
+        name: newFranchise.name,
+        admins: [
+            {
+                email: adminEmail,
+                id: expect.any(Number), // Expect admin ID to be present
+                name: expect.any(String) // Expect admin name to be present
+            }
+        ],
+        id: expect.any(Number) // Expect franchise ID to be present
+    });
+});
 
 test('getUserFranchise', async () => {
     const getFranchiseRes = await request(app)
@@ -48,13 +75,7 @@ test('getUserFranchise', async () => {
                     email: expect.any(String) // Expect an admin email to be present
                 }
             ],
-            stores: [
-                {
-                    id: expect.any(Number), // Expect a store ID to be present
-                    name: expect.any(String), // Expect a store name to be present
-                    totalRevenue: expect.any(Number) // Expect total revenue to be present
-                }
-            ]
+            stores: []
         }
     ]);
 });
