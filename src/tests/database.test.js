98c7ce6d0f0a5f176a36f1e41a897b859a84bb7c
davidmolgard <molgarddavid@gmail.com>
2026-02-03 16:58:23 -0700

    renamed service.test.js to database.test.js

---
+++ src/tests/database.test.js
@@ -0,0 +1,18 @@
+const { Role, DB } = require('../database/database.js');
+
+async function createAdminUser() {
+  let user = { password: 'toomanysecrets', roles: [{ role: Role.Admin }] };
+  user.name = randomName();
+  user.email = user.name + '@admin.com';
+
+  user = await DB.addUser(user);
+  return { ...user, password: 'toomanysecrets' };
+}
+
+function randomName() {
+  return Math.random().toString(36).substring(2, 12);
+}
+
+if (process.env.VSCODE_INSPECTOR_OPTIONS) {
+  jest.setTimeout(60 * 1000 * 5); // 5 minutes
+}
